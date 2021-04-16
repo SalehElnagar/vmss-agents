f1a4a910f6088de374151d79f12787911439d6ac
saleh elnaggar <saleh.elnaggar@gmail.com>
2021-04-16 22:21:44 +0200

    Initial commit

---
+++ 1. network/variables.tf
@@ -0,0 +1,19 @@
+variable "project_code" {
+  default = "moog"
+}
+
+variable "environment_code" {
+  default = "dev"
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "network_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "agents_subnet_cidr" {
+  default = "10.0.0.0/24"
+}
